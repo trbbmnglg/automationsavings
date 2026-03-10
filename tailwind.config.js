f325f69c608b8bb0f8c3c9daa9402f9996da8bd9
trbbmnglg <65166278+trbbmnglg@users.noreply.github.com>
2026-03-10 13:01:18 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electric-violet': {
+          50: '#f9f5ff',
+          100: '#f3ebff',
+          200: '#e4ccff',
+          300: '#d09eff',
+          400: '#b861ff',
+          500: '#9d24ff',
+          600: '#8900ff',
+          700: '#7300d6',
+          800: '#5e00a8',
+          900: '#4a0080',
+          950: '#2b0054',
+        }
+      },
+      fontFamily: {
+        sans: ['"Aptos Display"', 'system-ui', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
